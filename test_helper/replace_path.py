92239195840eee83a12abdf6909824372434bad2
Tina Jiang <tinayujiang@devfair0161.h2.fair>
2018-02-02 00:07:42 -0700

    remove ignore file

--- test_helper/replace_path.py
+++
@@ -1,7 +0,0 @@
-import string
-
-
-def replace_to_my_mac(old_path):
-    #new_str = old_path.replace('/private/home/tinayujiang/n2nmn/',
-                             #'/Users/tinayujiang/work/github/n2nmn_pytorch/')
-    return old_path
