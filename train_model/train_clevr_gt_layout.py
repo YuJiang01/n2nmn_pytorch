1d264b0207fdee4d1455d3a44371720ece574385
Yu Jiang <tinayujiang@fb.com>
2018-02-14 11:58:43 -0800

    add decayed accuracy

--- train_model/train_clevr_gt_layout.py
+++ train_model/train_clevr_gt_layout.py
@@ -186,6 +186,7 @@ for i_iter, batch in enumerate(data_reader_trn.batches()):
         print("iter:", i_iter + 1,
               " cur_layout_accuracy:%.3f"% current_layout_accuracy, " avg_layout_accuracy:%.3f"% avg_layout_accuracy,
               " cur_ans_accuracy:%.4f"% current_answer_accuracy, " avg_answer_accuracy:%.4f"% avg_answer_accuracy,
+              " decay_answer_accuracy:%.4f" % avg_accuracy,
               " layout_loss:%.3f"% layout_loss.cpu().data.numpy()[0], "answer_loss: %.3f"% answer_loss.cpu().data.numpy()[0])
         sys.stdout.flush()
 
