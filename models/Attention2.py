fix double softmax bug

models/Attention2.py
@@ -33,7 +33,7 @@ class EncoderRNN(nn.Module):
 
 class AttnDecoderRNN(nn.Module):
     def __init__(self, hidden_size, output_size, output_encoding_size,
-                 max_decoder_len=10, dropout_p=0.1,num_layers = 1,
+                 max_decoder_len=0, dropout_p=0.1,num_layers = 1,
                  assembler_w=None, assembler_b=None, assembler_p = None,EOStoken=-1):
         super(AttnDecoderRNN, self).__init__()
         self.hidden_size = hidden_size
@@ -114,15 +114,18 @@ class AttnDecoderRNN(nn.Module):
                                         encoder_outputs, encoder_lens, decoding_state):
         ##step1 run LSTM to get decoder hidden state
 
-        if time ==0:
+
+
+        seq_len = encoder_outputs.size(0)
+        batch_size = encoder_outputs.size(1)
+        hidden_size = encoder_outputs.size(2)
+
+        if time == 0 and seq_len == 1:
             embedded = self.go_embeding(previous_token)
         else:
             embedded = self.embedding(previous_token)
             embedded = self.dropout(embedded)
 
-        seq_len = encoder_outputs.size(0)
-        batch_size = encoder_outputs.size(1)
-        hidden_size = encoder_outputs.size(2)
         out_len = embedded.size(0)
 
         output, hidden = self.lstm(embedded, previous_hidden_state)
@@ -171,13 +174,18 @@ class AttnDecoderRNN(nn.Module):
         ## [out_len,batch,out_size]
         output_prob = F.softmax(self.out(combined), dim=2)
 
+
+        ## here we need to compute the predicted_tokens and log(P_selected_token)
+
+
         ##get the valid token for current position based on previous token to perform a mask for next prediction
         ## token_validity [N, output_size]
         token_invalidity = self._get_valid_tokens(decoding_state=decoding_state,
                                                 assembler_W=self.assembler_w,
                                                 assembler_b=self.assembler_b)
 
-        probs = F.softmax(output_prob,dim=1).view(-1,self.output_size)
+        ## probs
+        probs = output_prob.view(-1,self.output_size)
         probs.data.masked_fill_(token_invalidity,0.0)
         probs_sum = torch.sum(probs, dim=1, keepdim=True)
         probs = probs/probs_sum

models/custom_loss.py
@@ -6,11 +6,15 @@ class custom_loss(nn.Module):
         super(custom_loss, self).__init__()
         self.lambda_entropy = lambda_entropy
 
-    def forward(self, neg_entropy, answer_loss, policy_gradient_losses):
+    def forward(self, neg_entropy, answer_loss, policy_gradient_losses=None,layout_loss =None):
         #answer = torch.mean(answer_loss)
         #entropy = torch.mean(neg_entropy)
         #policy_gradient = torch.mean(policy_gradient_losses)
         #print(" answer= %f, entropy  = %f, policy_gradient = %f" %
         #          (answer,entropy,policy_gradient))
-        return torch.mean(neg_entropy) * self.lambda_entropy \
-               + torch.mean(answer_loss) + torch.mean(policy_gradient_losses)
+
+        if layout_loss is None:
+            return torch.mean(neg_entropy) * self.lambda_entropy +\
+               torch.mean(answer_loss)+torch.mean(policy_gradient_losses)
+        else:
+            return torch.mean(answer_loss) + layout_loss

models/end2endModuleNet.py
@@ -10,7 +10,7 @@ from Utils.utils import unique_columns
 class end2endModuleNet(nn.Module):
     def __init__(self, num_vocab_txt, num_vocab_nmn, out_num_choices,
                 embed_dim_nmn, embed_dim_txt, image_height, image_width, in_image_dim,
-                hidden_size, assembler, layout_criterion, answer_criterion, num_layers=1, decoder_dropout=0.1):
+                hidden_size, assembler, layout_criterion, answer_criterion,max_layout_len, num_layers=1, decoder_dropout=0.1):
 
         super(end2endModuleNet, self).__init__()
 
@@ -18,9 +18,11 @@ class end2endModuleNet(nn.Module):
         self.layout_criterion = layout_criterion
         self.answer_criterion = answer_criterion
 
+
         ##initiate encoder and decoder
         myEncoder = EncoderRNN(num_vocab_txt, hidden_size, embed_dim_txt, num_layers)
         myDecoder = AttnDecoderRNN(hidden_size, num_vocab_nmn, embed_dim_nmn,
+                                   max_decoder_len = max_layout_len,
                                    dropout_p=decoder_dropout, num_layers= num_layers,
                                    assembler_w=self.assembler.W, assembler_b=self.assembler.b,
                                    assembler_p=self.assembler.P, EOStoken=self.assembler.EOS_idx)
@@ -53,8 +55,12 @@ class end2endModuleNet(nn.Module):
         myLayouts, myAttentions, neg_entropy, log_seq_prob = self.mySeq2seq(input_txt_variable, input_text_seq_lens, input_layout_variable)
 
 
-        ##compute layout Loss
-        #layout_loss = self.layout_criterion(log_seq_prob=log_seq_prob,neg_entropy=neg_entropy)
+        log_seq_prob_avg = torch.mean(log_seq_prob)
+        print("log_seq_prob_avg= %f"%(log_seq_prob_avg))
+        layout_loss = None
+        if input_layout_variable is not None:
+            layout_loss = torch.mean(-log_seq_prob)
+
 
         predicted_layouts = np.asarray(myLayouts.cpu().data.numpy()) #torch.topk(myLayouts, 1)[1].cpu().data.numpy()[:, :, 0]
         expr_list, expr_validity_array = self.assembler.assemble(predicted_layouts)
@@ -99,7 +105,8 @@ class end2endModuleNet(nn.Module):
                 sample_group_tensor = torch.cuda.LongTensor(sample_group) if use_cuda else torch.LongTensor(sample_group)
                 
                 current_log_seq_prob = log_seq_prob[sample_group_tensor]
-                tmp1 = current_answer_loss.detach() - policy_gradient_baseline
+                current_answer_loss_val = Variable(current_answer_loss.data,requires_grad=False)
+                tmp1 = current_answer_loss_val - policy_gradient_baseline
                 current_policy_gradient_loss = tmp1 * current_log_seq_prob
 
                 if answer_losses is None:
@@ -111,8 +118,8 @@ class end2endModuleNet(nn.Module):
 
                 current_answer[sample_group] = torch.topk(myAnswers, 1)[1].cpu().data.numpy()[:, 0]
 
-        total_loss = self.layout_criterion(neg_entropy=neg_entropy,
-                                           answer_loss=answer_losses, policy_gradient_losses=policy_gradient_losses)
+        total_loss = self.layout_criterion(neg_entropy=neg_entropy, answer_loss=answer_losses,
+                                           policy_gradient_losses=policy_gradient_losses,layout_loss=layout_loss)
 
         ##update layout policy baseline
         avg_sample_loss = torch.mean(answer_losses)